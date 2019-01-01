3a4645d1bc71f8bd2ba7f975ffd9b9201a7af376
Joseph Martinez <josephsmartinez@gmail.com>
2018-12-31 23:53:30 -0500

    sample and level2 exercises

---
+++ programming_fundamentals/sample.go
@@ -0,0 +1,24 @@
+package main
+import (
+  "fmt"
+)
+
+var (
+  a int = 43
+  b float64 = 43.2
+  c string = "hello there"
+)
+
+const (
+  d int = 43
+  e float64 = 43.2
+  f string = "hello there"
+)
+
+func main() {
+
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+
+}
