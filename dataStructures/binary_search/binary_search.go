66cd83a73ce5ec9b2d4234ca8e659bd567bccca8
Joseph Martinez <josephsmartinez@gmail.com>
2018-12-31 21:24:55 -0500

    binary search fixed

--- dataStructures/binary_search/binary_search.go
+++ dataStructures/binary_search/binary_search.go
@@ -1,30 +1,31 @@
 package main
+
 import "fmt"
 
 // Function return a boolen value when number is found.
 func binarySearch(needle int, haystack []int) bool {
-  low := 0
-  high := len(haystack) - 1
+	low := 0
+	high := len(haystack) - 1
 
-  for low <= high{
-    median := (low + high) / 2
+	for low <= high {
+		median := (low + high) / 2
 
-    if haystack[median] < needle {
-      low = median + 1
-    }else{
-      high = median - 1
-    }
+		if haystack[median] < needle {
+			low = median + 1
+		} else {
+			high = median - 1
+		}
+	}
+  // if the list is empty OR the first element is NOT the equal to the needle.
+	if low == len(haystack) || haystack[low] != needle {
+		return false
+	}
 
-    if low == len(haystack) {
-      return false
-    }
-  }
-
-  return true
+	return true
 
 }
 
 func main() {
-  items := []int{1, 2, 9, 20, 31, 45, 63, 70, 100}
-  fmt.Println(binarySearch(63, items))
+	items := []int{1, 2, 9, 20, 31, 45, 63, 70, 100}
+	fmt.Println(binarySearch(63, items))
 }
